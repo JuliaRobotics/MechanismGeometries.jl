e265e486e20e02e45303c5b9a2102020d1c807a8
Robin Deits <robin.deits@gmail.com>
2018-03-15 17:13:43 -0400

    fix submechanism handling

--- test/runtests.jl
+++ test/runtests.jl
@@ -136,6 +136,16 @@ homog(t::Translation) = homog(AffineMap(eye(3), t.v))
             @test element.color == RGBA(0, 0, 1, 1)
             @test homog(element.transform) ≈ homog(Translation(0, 0, -1))
         end
+
+        @testset "acrobot submechanism" begin
+            urdf = "urdf/Acrobot.urdf"
+            robot = parse_urdf(Float64, urdf)
+            s, _ = submechanism(robot, bodies(robot)[3])
+            elements = visual_elements(s, URDFVisuals(urdf))
+            @test length(elements) == 2
+            @test string(rbd.body_fixed_frame_to_body(robot, elements[1].frame)) == "upper_link" 
+            @test string(rbd.body_fixed_frame_to_body(robot, elements[2].frame)) == "lower_link" 
+        end
     end
 
     @testset "valkyrie" begin
